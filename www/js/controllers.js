de2375b4889fc305ca1a4f73043ed2baf7123e1d
kowalsska <2037342k@student.gla.ac.uk>
2015-11-12 22:50:20 +0000

    Home screen done
    Buttons are not active yet

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,3 +1,13 @@
 /**
  * Created by magdakowalska on 12/11/2015.
  */
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope, $state){
+
+
+})
+
+.controller('FriendsCtrl', function($scope, $state){
+
+});
